fa6eef815fc47caef7f9b705ec7809f1ee8810df
udasitharani <udasitharani@gmail.com>
2020-03-24 17:41:24 +0530

    Completed the flaskapp  and added email verification method in email_sender.py

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 from flask import Flask
+import email_sender
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "HELLO WORLD!"
+
+##########Enter sender email here########
+sender=""          
+pw = ""            
+##########Enter password here############
+
+
+htmlFile = open("otp.html")
+html=htmlFile.read()
+body = {
+    "subject": "Verification Code",
+    "html": html
+}
+
+
+@app.route('/', methods=['GET'])
+def home(name=None, receiver=None):
+    if(name==None and receiver==None):
+        return "Error 00392: No name and receiver specified"
+    else:
+        code = email_sender.sendOTP(sender, pw, receiver, name, body)
+        return code
+
+
 
 if(__name__=="__main__"):
     app.run()
--- email_sender.py
+++ email_sender.py
@@ -1,14 +1,33 @@
-import ssl, smtplib
+import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import os, random
+import os, random, re
 
+
+def validate_email(email):
+        if(len(email)):
+            symbol = email.find('@')
+            if(symbol<1):
+                return False
+            recipient = [0, symbol-1]
+            if((len(email[recipient[0]:recipient[1]+1])<=64) and (len(email[recipient[0]:recipient[1]+1])>0)):
+                tmp = re.findall("^[a-zA-z0-9]*[a-zA-z0-9!#\$%&'\*\+-/=?\^_`\{\}\|\"\(\),:;<>\[\]\.\\]*[a-zA-z0-9]*$", email[recipient[0]:recipient[1]+1])
+            else:
+                return False
+            if(len(tmp)==0):
+                return False
+            domain = symbol+1           
+            if((len(email[domain:])>3) and (len(email[domain:])<=253)):
+                tmp = re.findall("^[a-zA-z0-9]*[a-zA-z0-9!#\$%&'\*\+-/=?\^_`\{\}\|\"\(\),:;<>\[\]\.\\]*[a-zA-z0-9]*$", email[domain:])
+            return True
+        return False
 
 def sendOTP(sender, pw, receiver_address, name, body):
     smtp_server = "smtp.gmail.com"
     port = 587
     sender_address = sender
-    
+    if(not validate_email(receiver_address)):
+        return "EMAIL101"
     message = MIMEMultipart("alternative")
     message["Subject"] = body['subject']
     message["From"] = sender_address
@@ -33,17 +52,17 @@ def sendOTP(sender, pw, receiver_address, name, body):
     message.attach(part1)
     message.attach(part2)
 
-    context = ssl.create_default_context()
+    # context = ssl.create_default_context()
 
     try:
         server = smtplib.SMTP(smtp_server, port)
-        server.ehlo()
-        server.starttls(context=context)
-        server.ehlo()
+        # server.ehlo()
+        server.starttls()
+        # server.ehlo()
         server.login(sender_address, pw)
         server.sendmail(sender_address, receiver_address, message.as_string())
-        return code
+        return str(code)
     except Exception as e:
-        print(e)
+        return e.message
     finally:
         server.quit()
